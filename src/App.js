b2ea73f06b4a62405e86d02db1c1085dcfe63dc9
Aroarafat <arafathossainbcpsc@gmail.com>
2021-09-21 15:47:04 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyComponent brand='apple' price='50000'></MyComponent>
+      <MyComponent brand='samsung' price='40000'></MyComponent>
+      <MyComponent brand='google' price='30000'></MyComponent>
+      <MyComponent brand='xiaomi' price='20000'></MyComponent>
     </div>
   );
 }
 
+
+function MyComponent(props) {
+  console.log(props);
+  const myStyle = {
+    backgroundColor: 'skyblue',
+    border: '4px solid blue',
+    margin: '20px',
+    padding: '10px',
+    borderRadius: '25px'
+  }
+  return (
+    <div style={myStyle}>
+      <h1>Brand-Name:{props.brand}</h1>
+      <p style={{ color: 'white', fontWeight: 'bold' }}>Price:{props.price}</p>
+    </div>
+  )
+}
 export default App;
